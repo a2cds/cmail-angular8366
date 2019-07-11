771996eed6f933c8bab0b8041c6743d0cf00b1aa
a2cds <acdasilva@gmail.com>
2019-07-11 20:22:25 -0300

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,7 +8,8 @@ import { CadastroComponent } from './modules/cadastro/cadastro.component';
 const rotasApp: Routes = [
     {path: '', component: LoginComponent},
     {path: 'inbox', component: CaixaDeEntradaComponent},
-    {path: 'cadastro', component: CadastroComponent}
+    {path: 'cadastro', component: CadastroComponent},
+    {path: 'login', component: LoginComponent}
 ]
 
 export const RoteamentoModule = RouterModule.forRoot(rotasApp);
